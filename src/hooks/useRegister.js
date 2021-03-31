d9bf7359e422ca63b34d5f46c1ee18bdf3ce2341
Deepak Priyadarshi <myselfdeepak9405@gmail.com>
2021-04-01 00:48:35 +0530

    Created Registration Form Hook

---
+++ src/hooks/useRegister.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import validator from "validator";
+
+function useRegister() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [dob, setDob] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const initialErrorMessage = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        dob: "",
+        password: "",
+        confirmPassword: "",
+    };
+
+    const [errorMessage, setErrorMessage] = useState(initialErrorMessage);
+
+    const validateRegistrationForm = () => {
+        let valid = true;
+
+        if (validator.isEmpty(firstName)) {
+            setErrorMessage({ ...errorMessage, firstName: "Please Enter First Name" });
+            valid = false;
+        } else if (validator.isEmpty(lastName)) {
+            setErrorMessage({ ...errorMessage, lastName: "Please Enter Last Name" });
+            valid = false;
+        } else if (validator.isEmpty(email)) {
+            setErrorMessage({ ...errorMessage, email: "Please Enter Email" });
+            valid = false;
+        } else if (validator.isEmpty(password)) {
+            setErrorMessage({ ...errorMessage, password: "Please Enter Password" });
+            valid = false;
+        } else if (validator.isEmpty(confirmPassword)) {
+            setErrorMessage({ ...errorMessage, confirmPassword: "Please Confirm Your Password" });
+            valid = false;
+        } else if (password.length !== confirmPassword.length || password !== confirmPassword) {
+            setErrorMessage({
+                ...errorMessage,
+                password: "Password Does Not Match With Confirm Password",
+                confirmPassword: "Confirm Password Does Not Match With Password",
+            });
+            valid = false;
+        }
+
+        if (valid) setErrorMessage(initialErrorMessage);
+
+        return valid;
+    };
+
+    const resetForm = () => {
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setDob("");
+        setPassword("");
+        setConfirmPassword("");
+
+        setErrorMessage(initialErrorMessage);
+    };
+
+    return [
+        firstName,
+        lastName,
+        email,
+        dob,
+        password,
+        confirmPassword,
+        errorMessage,
+        setFirstName,
+        setLastName,
+        setEmail,
+        setDob,
+        setPassword,
+        setConfirmPassword,
+        validateRegistrationForm,
+        resetForm,
+    ];
+}
+
+export default useRegister;
