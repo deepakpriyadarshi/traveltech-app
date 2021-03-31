9a9b8cdf48a8e92ad51fde09ee120adbd725745d
Deepak Priyadarshi <myselfdeepak9405@gmail.com>
2021-04-01 00:57:26 +0530

    Used Registration Hook On Form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -2,10 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import registerSVG from "../../assets/images/register.svg";
+import useRegister from "../../hooks/useRegister";
 
 import "./index.css";
 
 function Register() {
+    const [
+        firstName,
+        lastName,
+        email,
+        dob,
+        password,
+        confirmPassword,
+        errorMessage,
+        setFirstName,
+        setLastName,
+        setEmail,
+        setDob,
+        setPassword,
+        setConfirmPassword,
+        validateRegistrationForm,
+        resetForm,
+    ] = useRegister();
+
+    const registerUser = () => {
+        console.log("VALID", validateRegistrationForm());
+
+        console.log(firstName, lastName, email, dob, password, confirmPassword);
+    };
+
+    const clearForm = () => {
+        resetForm();
+    };
+
     return (
         <div className="container-fluid">
             <div className="row register-section">
@@ -21,45 +50,60 @@ function Register() {
                         <label>
                             First Name <span className="text-danger">*</span>
                         </label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" value={firstName} onChange={(ev) => setFirstName(ev.target.value)} />
+                        <span className="text-danger">{errorMessage.firstName}</span>
                     </div>
 
                     <div className="form-group">
                         <label>
                             Last Name <span className="text-danger">*</span>
                         </label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" value={lastName} onChange={(ev) => setLastName(ev.target.value)} />
+                        <span className="text-danger">{errorMessage.lastName}</span>
                     </div>
 
                     <div className="form-group">
                         <label>
                             Email <span className="text-danger">*</span>
                         </label>
-                        <input type="email" className="form-control" />
+                        <input type="email" className="form-control" value={email} onChange={(ev) => setEmail(ev.target.value)} />
+                        <span className="text-danger">{errorMessage.email}</span>
                     </div>
 
                     <div className="form-group">
                         <label>DOB</label>
-                        <input type="date" className="form-control" />
+                        <input type="date" className="form-control" value={dob} onChange={(ev) => setDob(ev.target.value)} />
+                        <span className="text-danger">{errorMessage.dob}</span>
                     </div>
 
                     <div className="form-group">
                         <label>
                             Password <span className="text-danger">*</span>
                         </label>
-                        <input type="password" className="form-control" />
+                        <input type="password" className="form-control" value={password} onChange={(ev) => setPassword(ev.target.value)} />
+                        <span className="text-danger">{errorMessage.password}</span>
                     </div>
 
                     <div className="form-group">
                         <label>
                             Confirm Password <span className="text-danger">*</span>
                         </label>
-                        <input type="password" className="form-control" />
+                        <input
+                            type="password"
+                            className="form-control"
+                            value={confirmPassword}
+                            onChange={(ev) => setConfirmPassword(ev.target.value)}
+                        />
+                        <span className="text-danger">{errorMessage.confirmPassword}</span>
                     </div>
 
                     <div className="text-right">
-                        <button className="btn btn-danger mr-5">Reset</button>
-                        <button className="btn btn-success">Register</button>
+                        <button className="btn btn-danger mr-5" onClick={clearForm}>
+                            Reset
+                        </button>
+                        <button className="btn btn-success" onClick={registerUser}>
+                            Register
+                        </button>
                     </div>
                 </div>
             </div>
